f541d4b2e13fe3a8321d13b02e8314acbd5d07ac
Marlon Hodgson <marlonhodgsonm@gmail.com>
2026-01-24 04:28:49 +0000

    Domain Name Generator Optimized

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Variables declaradas en el ámbito global para que todas las funciones puedan acceder
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Domain Name Generator by Hodgson Dev LLC");
+
+  // Mostrar todas las combinaciones en consola
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        console.log(domain);
+      }
+    }
+  }
+
+  // Generar el primer dominio inmediatamente
+  generateDomainNames();
+  
+  // Generar un nuevo dominio cada 3 segundos
+  setInterval(generateDomainNames, 3000);
 };
+
+function generateDomainNames() {
+  let randomPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  let randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  let randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  
+  // Actualizar el DOM (sin return antes para que esta línea SÍ se ejecute)
+  document.querySelector("#domain-name").innerText = randomPronoun + randomAdj + randomNoun + ".com";
+}
